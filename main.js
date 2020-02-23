5b323bd906e1131b8260076b9b07da32cea7851a
brokyo <alexcarusillo@gmail.com>
2020-02-23 10:36:08 -0500

    electron autoupdater

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 const {app, BrowserWindow, ipcMain} = require('electron')
+const { autoUpdater } = require('electron-updater')
 const path = require('path')
 require(path.join(__dirname, '/app/server/server.js'))
 
 function createWindow () {
   const mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 1200,
-    fullscreen:true,
+    width: 600,
+    height: 600,
+    // fullscreen:true,
     webPreferences: {
+      nodeIntegration: true,
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
   mainWindow.loadURL('http://localhost:3000/')
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
+
+  mainWindow.once('ready-to-show', _ => {
+    console.log('ready to show')
+    autoUpdater.checkForUpdatesAndNotify()
+  })
 }
 
 app.on('ready', createWindow)
@@ -26,5 +33,18 @@ app.on('activate', function () {
   if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+ipcMain.on('app_version', event => {
+  event.sender.send('app_version', {version: app.getVersion()})
+})
+
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
+});
+
+autoUpdater.on('update-available', _ => {
+  mainWindow.webContents.send('updates_available')
+})
+
+autoUpdater.on('update-downloaded', _ => {
+  mainWindow.webContents.send('updates_downloaded')
+})
