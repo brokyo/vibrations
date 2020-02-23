1445b4ca86e3effd2da0e548cf897b02140b0d9c
brokyo <alexcarusillo@gmail.com>
2020-02-23 09:58:49 -0500

    changed chroma assignment

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
 const path = require('path')
 require(path.join(__dirname, '/app/server/server.js'))
 
@@ -13,26 +13,16 @@ function createWindow () {
   })
 
   mainWindow.loadURL('http://localhost:3000/')
-
-  // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
 
